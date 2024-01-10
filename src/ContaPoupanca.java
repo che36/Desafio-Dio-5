8546d1a9b17155afa2223105e11467cf4800ef78
Neo40 <tron36.cc@gmail.com>
2024-01-10 18:35:04 -0300

    Adição de métodos

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -2,4 +2,16 @@ public class ContaPoupanca {
     public int agencia;
     public int numero;
     public double saldo;
+
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
 }
