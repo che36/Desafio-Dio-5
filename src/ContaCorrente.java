81128540d44fdfb82647ec6e8434f3f40c09befa
Neo40 <tron36.cc@gmail.com>
2024-01-16 22:15:37 -0300

    Adição de construtor e constante estática com relação à classe

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,2 +1,8 @@
 public class ContaCorrente extends Conta {
+    private static int SEQUENCIAL = 0;
+
+    public ContaCorrente() {
+        super.agencia = 1;
+        super.numero = SEQUENCIAL;
+    }
 }
