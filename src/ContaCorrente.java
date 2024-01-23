f83140f94efcaded30fc54731febdf43b0c245ac
Neo40 <tron36.cc@gmail.com>
2024-01-23 18:19:04 -0300

    Refactor de método

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -2,9 +2,9 @@ public class ContaCorrente extends Conta {
 
     @Override
     public void imprimirExtrato() {
-        System.out.println("=== Extrato Conta Corrente === ");
-        System.out.println(String.format("Agencia:%d", super.agencia));
-        System.out.println(String.format("Conta:%d", super.numero));
-        System.out.println(String.format("Saldo:%.2f", super.saldo));
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
     }
+
+
 }
