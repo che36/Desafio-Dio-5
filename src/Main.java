e501cb8cbcd9f85396069e6e6182e9c3ac777536
Neo40 <tron36.cc@gmail.com>
2024-01-24 22:16:06 -0300

    uso de método pelos objetos criados

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@
 public class Main {
     public static void main(String[] args) {
         Conta cc = new ContaCorrente();
+        cc.depositar(100);
         Conta cp = new ContaPoupanca();
-        System.out.println();
-        System.out.println();
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
     }
+
 }
