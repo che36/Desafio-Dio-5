91a3ab3e57856813a45944e9052826154e34b25a
Neo40 <tron36.cc@gmail.com>
2024-01-26 20:54:33 -0300

    Impressao do metodo tranferir usando objeto conta corrente

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Conta cc = new ContaCorrente();
         cc.depositar(100);
-        Conta cp = new ContaPoupanca();
         cc.imprimirExtrato();
+        Conta cp = new ContaPoupanca();
+        cc.tranferir(100, cp);
         cp.imprimirExtrato();
     }
 
