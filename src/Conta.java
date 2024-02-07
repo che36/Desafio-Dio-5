7bbff65c0b3956a576e811e5a8e47a287b4bddb7
Neo40 <tron36.cc@gmail.com>
2024-02-07 18:46:04 -0300

    Correção da saida da formatação

--- src/Conta.java
+++ src/Conta.java
@@ -42,7 +42,7 @@ public abstract class Conta implements IConta {
     }
 
     protected void imprimirInfosComuns() {
-        System.out.println(String.format("Titular:%d", this.cliente.getNome()));
+        System.out.println(String.format("Titular:%s", this.cliente.getNome()));
         System.out.println("=== Extrato Conta Corrente === ");
         System.out.println(String.format("Agencia:%d", this.agencia));
         System.out.println(String.format("Conta:%d", this.numero));
