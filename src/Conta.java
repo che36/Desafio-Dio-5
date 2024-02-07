9dd06a43ff475f277b1ff459dadc29770d7e8e4b
Neo40 <tron36.cc@gmail.com>
2024-02-07 18:43:45 -0300

    Adição de get e formataçao do nome

--- src/Conta.java
+++ src/Conta.java
@@ -1,27 +1,16 @@
 public abstract class Conta implements IConta {
-    private static final int AGENCIA_PADRAO = 0;
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
     protected int agencia;
     protected int numero;
     protected double saldo;
     protected Cliente cliente;
-    private static int SEQUENCIAL = 1;
+
 
     public Conta(Cliente cliente) {
         this.agencia = Conta.AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
         this.cliente = cliente;
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public double getSaldo() {
-        return saldo;
     }
 
     @Override
@@ -40,10 +29,24 @@ public abstract class Conta implements IConta {
         contaDestino.depositar(valor);
     }
 
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
     protected void imprimirInfosComuns() {
+        System.out.println(String.format("Titular:%d", this.cliente.getNome()));
         System.out.println("=== Extrato Conta Corrente === ");
         System.out.println(String.format("Agencia:%d", this.agencia));
         System.out.println(String.format("Conta:%d", this.numero));
         System.out.println(String.format("Saldo:%.2f", this.saldo));
     }
+
 }
